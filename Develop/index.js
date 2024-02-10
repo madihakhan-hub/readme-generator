cc33efb90d85cf32a4ebc8d9561e3b26c3e77cfc
madihakhan-hub <144630720+madihakhan-hub@users.noreply.github.com>
2024-02-09 22:34:17 -0600

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateREADME = require('./utils/generateREADME');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Provide a brief description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What are the installation instructions for this project?',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Provide the usage information for this project.',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What are the contribution guidelines for this project?',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Provide the test instructions for this project.',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
